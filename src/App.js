dbb63b5b871adbe588c00011a86ff13348c2bba8
Yankyyyy <tamocha44@gmail.com>
2024-08-01 13:23:31 +0530

    feat: added color to UI

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import React, { useState } from 'react';
 import WetWellForm from './components/WetWellForm';
 import DesignOutput from './components/DesignOutput';
 import { calculateDesign } from './utils/CalculateDesign';
-import { Container, CssBaseline, Typography } from '@mui/material';
+import { Container, CssBaseline, Typography, Paper, Box } from '@mui/material';
+import { blue } from '@mui/material/colors';
 
 const App = () => {
   const [outputs, setOutputs] = useState(null);
@@ -15,13 +16,40 @@ const App = () => {
   };
 
   return (
-    <Container component="main" maxWidth="md">
+    <Container component="main">
       <CssBaseline />
-      <Typography variant="h4" align="center" gutterBottom>
-        <b>Wet Well Design Calculator</b>
-      </Typography>
-      <WetWellForm onCalculate={handleCalculate} />
-      {outputs && <DesignOutput outputs={outputs} />}
+      <Box
+        sx={{
+          my: 4,
+          p: 3,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          borderRadius: 2,
+        }}
+      >
+        <Paper
+          elevation={6}
+          sx={{
+            p: 4,
+            borderRadius: 2,
+            textAlign: 'center',
+            width: '100%',
+            maxWidth: 600,
+            backgroundColor: blue[50], // Light blue background for the Paper
+            border: `1px solid ${blue[300]}`, // Light blue border
+          }}
+        >
+          <Typography
+            variant="h4"
+            gutterBottom
+          >
+            <b>Wet Well Design Calculator</b>
+          </Typography>
+          <WetWellForm onCalculate={handleCalculate} />
+          {outputs && <DesignOutput outputs={outputs} />}
+        </Paper>
+      </Box>
     </Container>
   );
 };
