51aee4cb881c09b1a65026d04ccda5e0682574c8
Yankyyyy <tamocha44@gmail.com>
2024-08-01 11:20:05 +0530

    feat:input value validations added

--- src/components/WetWellForm.js
+++ src/components/WetWellForm.js
@@ -1,4 +1,3 @@
-// src/components/WetWellForm.js
 import React, { useState } from 'react';
 import { TextField, Button, Grid, Box, Typography } from '@mui/material';
 
@@ -14,6 +13,8 @@ const WetWellForm = ({ onCalculate }) => {
     deliveryHeadM: ''
   });
 
+  const [errors, setErrors] = useState({});
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setInputs({
@@ -22,9 +23,44 @@ const WetWellForm = ({ onCalculate }) => {
     });
   };
 
+  const validateInputs = (inputs) => {
+    const errors = {};
+    if (!inputs.averageFlowMLD || inputs.averageFlowMLD <= 0) {
+      errors.averageFlowMLD = 'Average Flow must be greater than 0';
+    }
+    if (!inputs.peakFactor || inputs.peakFactor <= 0) {
+      errors.peakFactor = 'Peak Factor must be greater than 0';
+    }
+    if (!inputs.efficiency || inputs.efficiency <= 0 || inputs.efficiency > 100) {
+      errors.efficiency = 'Efficiency must be between 0 and 100';
+    }
+    if (!inputs.timeForOnePumpCycle || inputs.timeForOnePumpCycle <= 0) {
+      errors.timeForOnePumpCycle = 'Time for one pump cycle must be greater than 0';
+    }
+    if (!inputs.minDepthBelowInvertM || inputs.minDepthBelowInvertM <= 0) {
+      errors.minDepthBelowInvertM = 'Minimum Depth Below Invert must be greater than 0';
+    }
+    if (!inputs.diameterOfInvertPipeM || inputs.diameterOfInvertPipeM <= 0) {
+      errors.diameterOfInvertPipeM = 'Diameter of Invert Pipe must be greater than 0';
+    }
+    if (!inputs.suctionHeadM || inputs.suctionHeadM <= 0) {
+      errors.suctionHeadM = 'Suction Head must be greater than 0';
+    }
+    if (!inputs.deliveryHeadM || inputs.deliveryHeadM <= 0) {
+      errors.deliveryHeadM = 'Delivery Head must be greater than 0';
+    }
+    return errors;
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    onCalculate(inputs);
+    const validationErrors = validateInputs(inputs);
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      setErrors({});
+      onCalculate(inputs);
+    }
   };
 
   return (
@@ -42,6 +78,8 @@ const WetWellForm = ({ onCalculate }) => {
             type="number"
             value={inputs.averageFlowMLD}
             onChange={handleChange}
+            error={!!errors.averageFlowMLD}
+            helperText={errors.averageFlowMLD}
           />
         </Grid>
         <Grid item xs={12} sm={6}>
@@ -53,6 +91,8 @@ const WetWellForm = ({ onCalculate }) => {
             type="number"
             value={inputs.peakFactor}
             onChange={handleChange}
+            error={!!errors.peakFactor}
+            helperText={errors.peakFactor}
           />
         </Grid>
         <Grid item xs={12} sm={6}>
@@ -64,6 +104,8 @@ const WetWellForm = ({ onCalculate }) => {
             type="number"
             value={inputs.minDepthBelowInvertM}
             onChange={handleChange}
+            error={!!errors.minDepthBelowInvertM}
+            helperText={errors.minDepthBelowInvertM}
             InputProps={{ inputProps: { step: 0.01 } }}
           />
         </Grid>
@@ -76,6 +118,8 @@ const WetWellForm = ({ onCalculate }) => {
             type="number"
             value={inputs.efficiency}
             onChange={handleChange}
+            error={!!errors.efficiency}
+            helperText={errors.efficiency}
             InputProps={{ inputProps: { step: 0.01 } }}
           />
         </Grid>
@@ -88,6 +132,8 @@ const WetWellForm = ({ onCalculate }) => {
             type="number"
             value={inputs.timeForOnePumpCycle}
             onChange={handleChange}
+            error={!!errors.timeForOnePumpCycle}
+            helperText={errors.timeForOnePumpCycle}
             InputProps={{ inputProps: { step: 0.01 } }}
           />
         </Grid>
@@ -100,6 +146,8 @@ const WetWellForm = ({ onCalculate }) => {
             type="number"
             value={inputs.suctionHeadM}
             onChange={handleChange}
+            error={!!errors.suctionHeadM}
+            helperText={errors.suctionHeadM}
             InputProps={{ inputProps: { step: 0.01 } }}
           />
         </Grid>
@@ -112,6 +160,8 @@ const WetWellForm = ({ onCalculate }) => {
             type="number"
             value={inputs.deliveryHeadM}
             onChange={handleChange}
+            error={!!errors.deliveryHeadM}
+            helperText={errors.deliveryHeadM}
             InputProps={{ inputProps: { step: 0.01 } }}
           />
         </Grid>
@@ -124,6 +174,8 @@ const WetWellForm = ({ onCalculate }) => {
             type="number"
             value={inputs.diameterOfInvertPipeM}
             onChange={handleChange}
+            error={!!errors.diameterOfInvertPipeM}
+            helperText={errors.diameterOfInvertPipeM}
             InputProps={{ inputProps: { step: 0.01 } }}
           />
         </Grid>
